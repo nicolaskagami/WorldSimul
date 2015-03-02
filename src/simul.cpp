c4d3a0fc060e3ac156f81ba195e4a719bd160a2e
Nicolassk <nicolaskagami@gmail.com>
2015-03-02 11:47:54 -0300

    Removing Humanity from simul.cpp

--- src/simul.cpp
+++ src/simul.cpp
@@ -325,37 +325,3 @@ void Map::print()
         printf("\n");
     }
 }
-Human::Human(int identification)
-{
-    id = identification;
-    age = 15 + rand() % 10;
-    sex = rand()%2;
-    happiness = 128;
-    health = 255;
-    nourishment = 128;
-}
-Human::~Human()
-{
-}
-void Human::print()
-{
-    printf("ID: %d, Sex: %d, Age: %d, Happiness: %d, Nourishment: %d, Health: %d\n",id, sex, age, happiness, nourishment, health);
-}
-Humanity::Humanity()
-{
-    int i;
-    humanity.reserve(INITIAL_POPULATION);
-    for(i=0;i<INITIAL_POPULATION;i++)
-    {
-        humanity.push_back(Human(i));
-    }
-}
-void Humanity::print()
-{
-    int i;
-    printf("SIZE: %d\n",humanity.size());
-    for(i=0;i < humanity.size();i++)
-    {
-        humanity[i].print();
-    }
-}
